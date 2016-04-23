2bc2ec935ae61e4ee16d6b0b99c0c6df948bf9c7
swiid <jacquesw@swiidappdesigns.com>
2016-04-23 02:02:15 +0200

    Start App
    Added first files

---
+++ Weather-TestTests/Weather_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_TestTests.swift
+//  Weather-TestTests
+//
+//  Created by Jacques Wud on 2016/04/23.
+//  Copyright © 2016 Jacques Wud. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Test
+
+class Weather_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
